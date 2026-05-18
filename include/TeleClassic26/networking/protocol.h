757ce4f5e72200ef3176596402ae9454d79aed2f
Michael Wang <mwang764@gatech.edu>
2026-05-17 21:00:30 -0700

    Added protocol send methods for block draw

--- include/TeleClassic26/networking/protocol.h
+++ include/TeleClassic26/networking/protocol.h
@@ -118,8 +118,6 @@ pboolean tc_cpe_send_custom_block_support_level(PSocket* session, pchar support_
 // - return: TRUE if the packet was sent, FALSE otherwise
 pboolean tc_cpe_send_set_texture_url(PSocket* session, const pchar texture_url[]);
 
-
-
 //map property values
 enum {
     TC_CPE_MAP_ENV_PROPERTY_SIDE_BLOCK = 0,
@@ -191,6 +189,94 @@ enum {
 // - blue: the blue value of the color to send the packet to
 // - return: TRUE if the packet was sent, FALSE otherwise
 pboolean tc_cpe_send_set_env_color(PSocket* session, pchar color_field, pint16 red, pint16 green, pint16 blue);
+
+// solidity values
+enum {
+    TC_CPE_SOLIDITY_WALK_THROUGH = 0, //i.e. air
+    TC_CPE_SOLIDITY_SWIM_THROUGH = 1, //i.e. water, lava
+    TC_CPE_SOLIDITY_SOLID = 2, //block collides with players
+    TC_CPE_SOLIDITY_PARTIALLY_SLIPPERY = 3, //player slides IFF on top (i.e. ice)
+    TC_CPE_SOLIDITY_FULLY_SLIPPERY = 4, //player slides IFF any touch contact
+    TC_CPE_SOLIDITY_WATER = 5, //behaves like water
+    TC_CPE_SOLIDITY_LAVA = 6, //behaves like lava
+    TC_CPE_SOLIDITY_ROPE = 7, //behaves like rope
+};
+
+// walk sound values
+enum {
+    TC_CPE_WALK_SOUND_NONE = 0,
+    TC_CPE_WALK_SOUND_WOOD = 1,
+    TC_CPE_WALK_SOUND_GRAVEL = 2,
+    TC_CPE_WALK_SOUND_GRASS = 3,
+    TC_CPE_WALK_SOUND_STONE = 4,
+    TC_CPE_WALK_SOUND_METAL = 5,
+    TC_CPE_WALK_SOUND_GLASS = 6,
+    TC_CPE_WALK_SOUND_WOOL = 7,
+    TC_CPE_WALK_SOUND_SAND = 8,
+    TC_CPE_WALK_SOUND_SNOW = 9
+};
+
+// block draw values
+enum {
+    TC_CPE_BLOCK_DRAW_OPAQUE = 0,
+    TC_CPE_BLOCK_DRAW_TRANSPARENT_GLASS = 1,
+    TC_CPE_BLOCK_DRAW_TRANSPARENT_LEAVES = 2,
+    TC_CPE_BLOCK_DRAW_TRANSLUCENT = 3,
+    TC_CPE_BLOCK_DRAW_GAS = 4
+};
+
+// sends a set block definition packet
+// - session: the session to send the packet to
+// - block_id: the id of the block to send the packet to
+// - block_name: the name of the block to send the packet to
+// - solidity: the solidity of the block to send the packet to
+// - movement_speed: the movement speed of the block to send the packet to
+// - top_texture_id: the id of the top texture to send the packet to
+// - bottom_texture_id: the id of the bottom texture to send the packet to
+// - side_texture_id: the id of the side texture to send the packet to
+// - transmits_light: the transmits light of the block to send the packet to
+// - walk_sound: the walk sound of the block to send the packet to
+// - full_bright: the full bright of the block to send the packet to
+// - shape: the shape of the block to send the packet to
+// - block_draw: the block draw of the block to send the packet to
+// - fog_density: the fog density of the block to send the packet to
+// - fog_color_red: the fog color red of the block to send the packet to
+// - fog_color_green: the fog color green of the block to send the packet to
+// - fog_color_blue: the fog color blue of the block to send the packet to
+
+// These are parameters that arent part of the packet
+// - use_extended_blocks: whether to use extended blocks
+// - use_extended_textures: whether to use extended textures
+
+// - return: TRUE if the packet was sent, FALSE otherwise
+pboolean tc_cpe_send_set_block_definition(
+    PSocket* session, 
+    puint16 block_id,
+    const pchar block_name[],
+    pchar solidity,
+    pchar movement_speed,
+    puint16 top_texture_id,
+    puint16 bottom_texture_id,
+    puint16 side_texture_id,
+    pchar transmits_light,
+    pchar walk_sound,
+    pchar full_bright,
+    pchar shape,
+    pchar block_draw,
+    pchar fog_density,
+    pchar fog_color_red,
+    pchar fog_color_green,
+    pchar fog_color_blue,
+
+    pboolean use_extended_blocks,
+    pboolean use_extended_textures
+);
+
+// removes a block definition packet
+// - session: the session to send the packet to
+// - block_definition: the block definition to remove the packet to
+// - return: TRUE if the packet was sent, FALSE otherwise
+pboolean tc_cpe_send_remove_block_definition(PSocket* session, puint16 block_id);
 
 // sends a message to the session
 // - session: the session to send the message to
